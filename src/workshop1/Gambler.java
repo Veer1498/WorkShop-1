d0021d45f4d8831eef980ae175ab216b9ed0dc9b
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-27 19:55:20 +0530

    Took Variables - Stake and Bet in Class Gambler

--- src/workshop1/Gambler.java
+++ src/workshop1/Gambler.java
@@ -8,7 +8,8 @@ public class Gambler {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*created Class Gambler and taken variables stake and bet.
+		/*Created Class Gambler and taken variables stake and bet with values.
+		 * The currency is in Dollars ($)
 		 * 
 		 */
 		int stake = 100;
