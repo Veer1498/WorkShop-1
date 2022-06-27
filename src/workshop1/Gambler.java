d7514ed4f2075605e78af576a20524a42c0e7ed7
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-27 20:47:24 +0530

    Gambler either Win or Loss a 50% of stake, Resign for the Day

--- src/workshop1/Gambler.java
+++ src/workshop1/Gambler.java
@@ -12,17 +12,31 @@ public class Gambler {
 		 * The currency is in Dollars ($)
 		 * Created an IF Condition for probability.
 		 * If random more than 1 it add up to stake, less than 1 it subtracts form stake.
+		 * Created a while loop and inserted all If Condition in That.
+		 * Created another IF condition to break the stake value at either 150 or 50.
+		 * 
+		 * 
 		 */
 		int stake = 100;
 		int bet = 1;
+		int count=0;
 		int random = (int) Math.floor((Math.random()*2)+1);
-		if (random>1) {
-			stake = stake + bet;
+		while(stake !=0) {
+			count++;
+			if (random>1) {
+				stake = stake + bet;
+				if (stake == 150 || stake == 50) {
+					break;
+				}
+			}
+			else {
+				stake = stake - bet;
+				if (stake == 150 || stake == 50) {
+					break;
+					}
+			}
 		}
-		else {
-			stake = stake - bet;
-		}
-		System.out.println("The stake Value after 1 Game = $ "+stake);
+		System.out.println("The stake Value after Game = $ "+stake);
 	}
 
 }
