b57d398779c5f168596b1b321eb662d4cc0072a8
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-27 19:48:26 +0530

    Created a Class Gambler

---
+++ src/workshop1/Gambler.java
@@ -0,0 +1,19 @@
+package workshop1;
+
+public class Gambler {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*created Class Gambler and taken variables stake and bet.
+		 * 
+		 */
+		int stake = 100;
+		int bet = 1;
+
+	}
+
+}
